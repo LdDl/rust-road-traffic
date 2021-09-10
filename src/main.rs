95ac45d46d4483d712946a05d9e148b32b67c94c
Dimitrii Lopanov <sexykdi@gmail.com>
2021-09-10 21:05:29 +0300

    imports

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use opencv::{
+    core,
+    prelude::*,
+    videoio,
+    imgcodecs::*,
+    imgproc::rectangle,
+    imgproc::resize,
+    dnn::Net,
+    dnn::DNN_BACKEND_CUDA,
+    dnn::DNN_TARGET_CUDA,
+    dnn::DNN_TARGET_CUDA_FP16,
+    dnn::read_net_from_tensorflow,
+    dnn::blob_from_image
+};
+
 fn main() {
     println!("Here will be opencv stuff soon");
 }
