35414fc63a36484a956619cfee7d4e6d9aec9fe2
Dimitrii Lopanov <sexykdi@gmail.com>
2021-09-11 15:25:37 +0300

    filter data from detections (before NMS)

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,21 @@ use opencv::{
     highgui,
     videoio,
     imgproc::resize,
+    imgproc::rectangle,
     dnn::DNN_BACKEND_CUDA,
     dnn::DNN_TARGET_CUDA,
     dnn::read_net,
-    dnn::blob_from_image
+    dnn::blob_from_image,
+    dnn::nms_boxes
 };
 
 use std::time::{Instant};
+use std::cmp;
 
 fn run() -> opencv::Result<()> {
     const OUTPUT_WIDTH: i32 = 500;
     const OUTPUT_HEIGHT: i32 = 500;
-    const CONF_THRESHOLD: f32 = 0.3;
+    const CONF_THRESHOLD: f32 = 0.1;
     const COCO_CLASSNAMES: &'static [&'static str] = &["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"];
     const CLASSES_NUM: usize = COCO_CLASSNAMES.len();
 
@@ -90,6 +93,16 @@ fn run() -> opencv::Result<()> {
     let mut resized_frame = core::Mat::default();
     let mut detections = core::Vector::<core::Mat>::new();
 
+    /* Read first frame to determine image width/height */
+    match video_capture.read(&mut frame) {
+        Ok(_) => {},
+        Err(_) => {
+            panic!("Can't read first frame");
+        }
+    };
+    let frame_cols = frame.cols() as f32;
+    let frame_rows = frame.rows() as f32;
+
     loop {
         match video_capture.read(&mut frame) {
             Ok(_) => {},
@@ -98,14 +111,8 @@ fn run() -> opencv::Result<()> {
                 break;
             }
         };
-        match resize(&mut frame, &mut resized_frame, core::Size::new(OUTPUT_WIDTH, OUTPUT_HEIGHT), 1.0, 1.0, 1) {
-            Ok(_) => {},
-            Err(err) => {
-                panic!("Can't resize output frame due the error {:?}", err);
-            }
-        }
 
-        let blobimg = blob_from_image(&frame, 0.00392, core::Size::new(608, 608), core::Scalar::default(), true, false, core::CV_32F);
+        let blobimg = blob_from_image(&frame, 1.0/255.0, core::Size::new(416, 416), core::Scalar::default(), true, false, core::CV_32F);
         match neural_net.set_input(&blobimg.unwrap(), "", 1.0, core::Scalar::default()){
             Ok(_) => {},
             Err(err) => {
@@ -116,12 +123,52 @@ fn run() -> opencv::Result<()> {
         let now = Instant::now();
         match neural_net.forward(&mut detections, &out_layers_names) {
             Ok(_) => {
+                let outs = detections.len();
+                // let mut class_ids = vec![];
+                // let mut confidences = vec![];
+                // let mut bboxes: vec![];
+                for o in 0..outs {
+                    let output = detections.get(o).unwrap();
+                    let data_ptr = output.data_typed::<f32>().unwrap();
+                    for (i, _) in data_ptr.iter().enumerate().step_by(CLASSES_NUM + 5) {
+                        let mut class = 0 as usize;
+                        let mut max_probability = 0.0;
+                        for j in 5..(CLASSES_NUM + 5) {
+                            if data_ptr[i+j] > max_probability {
+                                max_probability = data_ptr[i+j];
+                                class = (j-5) % CLASSES_NUM;
+                            }
+                        }
+                        if max_probability * data_ptr[i+4] > CONF_THRESHOLD {
+                            let center_x = data_ptr[i] * frame_cols;
+                            let center_y = data_ptr[i + 1] * frame_rows;
+                            let width = data_ptr[i + 2] * frame_cols;
+                            let height = data_ptr[i + 3] * frame_rows;
+                            let left = center_x - width / 2.0;
+                            let top = center_y - height / 2.0;
+                            let bbox = core::Rect::new(left as i32, top as i32, width as i32, height as i32);
+                            match rectangle(&mut frame, bbox, core::Scalar::new(255.0, 0.0, 0.0, 100.0), 2, 1, 0){
+                                Ok(_) => {},
+                                Err(err) => {
+                                    println!("Can't draw bounding box of object due the error {:?}", err);
+                                }
+                            };
+                        }
+                    }
+                }
             }
             Err(err) => {
                 println!("Can't process input of neural network due the error {:?}", err);
             }
         }
         println!("Elapes milliseconds to detect object on image: {}", now.elapsed().as_millis());
+
+        match resize(&mut frame, &mut resized_frame, core::Size::new(OUTPUT_WIDTH, OUTPUT_HEIGHT), 1.0, 1.0, 1) {
+            Ok(_) => {},
+            Err(err) => {
+                panic!("Can't resize output frame due the error {:?}", err);
+            }
+        }
 
         if resized_frame.size()?.width > 0 {
             highgui::imshow(window, &mut resized_frame)?;
