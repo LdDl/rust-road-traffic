ec18bd7ddf3d800ad9eb6f53e217cc5a5b3baa87
Dimitrii Lopanov <sexykdi@gmail.com>
2021-09-11 13:20:44 +0300

    initialize video capture

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use opencv::{
     core,
-    prelude::*,
+    highgui,
     videoio,
+    prelude::*,
     imgcodecs::*,
     imgproc::rectangle,
     imgproc::resize,
@@ -13,6 +14,45 @@ use opencv::{
     dnn::blob_from_image
 };
 
+fn run() -> opencv::Result<()> {
+    const OUTPUT_WIDTH: i32 = 500;
+    const OUTPUT_HEIGHT: i32 = 500;
+    let video_src = "./data/sample_960_540.mp4";
+    let window = "Tiny YOLO v4";
+    match highgui::named_window(window, 1) {
+        Ok(_) => {},
+        Err(err) =>{
+            panic!("Can't give a name to output window due the error: {:?}", err)
+        }
+    };
+    match highgui::resize_window(window, OUTPUT_WIDTH, OUTPUT_HEIGHT) {
+        Ok(_) => {},
+        Err(err) =>{
+            panic!("Can't resize output window due the error: {:?}", err)
+        }
+    }
+    println!("Available <videoio> backends: {:?}", videoio::get_backends()?);
+
+    // Check if CUDA is an option at all
+    let cuda_count = core::get_cuda_enabled_device_count()?;
+    let cuda_available = cuda_count > 0;
+    println!("CUDA is {}", if cuda_available { "available" } else { "not available" });
+    
+    let mut video_capture = match videoio::VideoCapture::from_file(video_src, videoio::CAP_ANY) {
+        Ok(result) => {result},
+        Err(err) => {
+            panic!("Can't init '{}' due the error: {:?}", video_src, err);
+        }
+    };
+
+    let opened = videoio::VideoCapture::is_opened(&video_capture)?;
+    if !opened {
+        panic!("Unable to open video '{}'", video_src);
+    }
+
+    Ok(())
+}
+
 fn main() {
-    println!("Here will be opencv stuff soon");
+    run().unwrap()
 }
