33bd3f161329b12edb343828b3e148f37a822963
Dimitrii Lopanov <sexykdi@gmail.com>
2021-09-24 14:48:57 +0300

    separate spatial

--- src/polygons/mod.rs
+++ src/polygons/mod.rs
@@ -1,4 +1,3 @@
 mod convex_polygons;
-mod spatial;
 
-pub use self::{convex_polygons::*, spatial::*};
+pub use self::{convex_polygons::*};
---
+++ src/spatial/mod.rs
@@ -0,0 +1,3 @@
+mod spatial;
+
+pub use self::{spatial::*};
