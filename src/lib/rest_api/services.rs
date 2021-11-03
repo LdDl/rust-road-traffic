bd5f4de93e3499c77b74edc5269768d2adf24b85
Dimitrii Lopanov <sexykdi@gmail.com>
2021-11-03 16:38:49 +0300

    add lane_num/dir info to stats REST

--- src/lib/rest_api/services.rs
+++ src/lib/rest_api/services.rs
@@ -28,11 +28,14 @@ use chrono::{DateTime, Utc};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct AllPolygonsStats {
+    pub equipment_id: String,
     pub data: Vec<PolygonStats>
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct PolygonStats {
+    pub lane_number: u16,
+    pub lane_direction: u8,
     pub period_start: DateTime<Utc>,
     pub period_end: Option<DateTime<Utc>>,
     pub statistics: HashMap<String, VehicleTypeParameters>
@@ -48,11 +51,14 @@ pub fn all_polygons_stats(data: web::Data<Arc<RwLock<HashMap<PolygonID, Mutex<Co
     let data_storage = data.get_ref().clone();
     let data_expected = data_storage.read().expect("expect: all_polygons_stats");
     let mut ans = AllPolygonsStats{
+        equipment_id: "".to_string(),
         data: vec![]
     };
     for (_, v) in data_expected.iter() {
         let element = v.lock().expect("Mutex poisoned");
         let mut stats = PolygonStats{
+            lane_number: element.road_lane_num,
+            lane_direction: element.road_lane_direction,
             period_start: element.period_start,
             period_end: element.period_end,
             statistics: HashMap::new()
