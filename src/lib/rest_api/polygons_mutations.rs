70b8f6429037a287c17809988a4bae18e056b208
Dimitrii <sexykdi@gmail.com>
2022-08-31 22:09:04 +0300

    replace all api

--- src/lib/rest_api/polygons_mutations.rs
+++ src/lib/rest_api/polygons_mutations.rs
@@ -203,3 +203,93 @@ pub async fn create_polygon(data: web::Data<Storage>, new_polygon: web::Json<Pol
         polygon_id: new_id
     }));
 }
+
+
+
+#[derive(Debug, Deserialize)]
+pub struct PolygonReplaceAllRequest {
+    pub data: Vec<PolygonCreateRequest>
+}
+
+#[derive(Debug, Serialize)]
+pub struct PolygonReplaceAllResponse {
+    pub polygons_ids: Vec<String>
+}
+
+//
+// curl -XPOST 'http://localhost:42001/api/mutations/replace_all' -d '{"data":[{"lane_number":0,"lane_direction":0,"pixel_points":[[1,1],[50,1],[50,50],[1,50]],"spatial_points":[[37.618908137083054,54.20564619851147],[37.61891517788172,54.20564502193819],[37.618927247822285,54.205668749493036],[37.61892020702362,54.2056701221611]],"color_rgb":[130,130,0]},{"lane_number":1,"lane_direction":0,"pixel_points":[[55,55],[105,55],[105,105],[55,105]],"spatial_points":[[37.618908137083054,54.20564619851147],[37.61891517788172,54.20564502193819],[37.618927247822285,54.205668749493036],[37.61892020702362,54.2056701221611]],"color_rgb":[130,0,130]}]}' -H 'Content-Type: application/json'
+//
+pub async fn replace_all(data: web::Data<Storage>, new_polygons: web::Json<PolygonReplaceAllRequest>) -> Result<HttpResponse, Error> {
+
+    if new_polygons.data.len() == 0 {
+        return Ok(HttpResponse::build(StatusCode::BAD_REQUEST).json(ErrorResponse {
+            error_text: "No polygons".to_string()
+        }));
+    }
+
+    // Mark data for clean
+    let data_storage = data.data_storage.as_ref().clone();
+    let data_expected = data_storage.read().expect("expect: polygons_list");
+    let data_expected_polygons = data_expected.polygons.read().expect("expect: polygons_list");
+    let need_to_clean: Vec<String> = data_expected_polygons.iter().map(|poly| poly.0.clone()).collect();
+    drop(data_expected_polygons);
+
+    // Add new data
+    let mut response = vec![];
+    for new_polygon in new_polygons.data.iter() {
+        let mut polygon = ConvexPolygon::empty();
+        match new_polygon.pixel_points {
+            Some(data) => {
+                polygon.update_pixel_map_arr(data);
+            },
+            _ => {}
+        }
+
+        match new_polygon.spatial_points {
+            Some(data) => {
+                polygon.update_spatial_map_arr(data);
+            },
+            _ => {}
+        }
+
+        match new_polygon.lane_direction {
+            Some(val) => {
+                polygon.set_road_lane_direction(val);
+            },
+            _ => {}
+        }
+
+        match new_polygon.lane_number {
+            Some(val) => {
+                polygon.set_road_lane_num(val);
+            },
+            _ => {}
+        }
+
+        match new_polygon.color_rgb {
+            Some(val) => {
+                polygon.set_color(val);
+            },
+            _ => {}
+        }
+
+        let new_id = polygon.get_id().clone();
+
+        let data_storage = data.data_storage.as_ref().clone();
+        let data_expected = data_storage.read().expect("expect: polygons_list");
+        data_expected.insert_polygon(polygon);
+
+        response.push(new_id);
+    }
+
+    // Clean data
+    let mut data_expected_polygons = data_expected.polygons.write().expect("expect: polygons_list");
+    for id in need_to_clean.iter() {
+        data_expected_polygons.remove(id);
+    }
+    drop(data_expected_polygons);
+
+    return Ok(HttpResponse::Ok().json(PolygonReplaceAllResponse{
+        polygons_ids: response
+    }));
+}
