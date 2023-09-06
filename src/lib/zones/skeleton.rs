e5155d0ccc2ab3fe7635267a183aed1c85f01992
Dimitrii <sexykdi@gmail.com>
2023-09-06 22:29:43 +0300

    reduce Point2i allocs for skeleton

--- src/lib/zones/skeleton.rs
+++ src/lib/zones/skeleton.rs
@@ -9,7 +9,8 @@ use opencv::{
 
 #[derive(Debug)]
 pub struct Skeleton {
-    line: [Point2f; 2],
+    line_cvf: [Point2f; 2],
+    line_cvi: [Point2i; 2],
     color: Scalar,
     pub length_pixels: f32,
     pub length_meters: f32,
@@ -20,7 +21,8 @@ impl Skeleton {
     pub fn new(a: Point2f, b: Point2f) -> Self {
         let length_pixels = ((a.x - b.x).powi(2) + (a.y - b.y).powi(2)).sqrt();
         Skeleton {
-            line: [a, b],
+            line_cvf: [a, b],
+            line_cvi: [Point2i::new(a.x as i32, a.y as i32), Point2i::new(b.x as i32, b.y as i32)],
             color: Scalar::from((0.0, 0.0, 0.0)),
             length_pixels: length_pixels,
             length_meters: -1.0,
@@ -29,7 +31,8 @@ impl Skeleton {
     }
     pub fn default() -> Self {
         Skeleton {
-            line: [Point2f::default(), Point2f::default()],
+            line_cvf: [Point2f::default(), Point2f::default()],
+            line_cvi: [Point2i::default(), Point2i::default()],
             color: Scalar::from((0.0, 0.0, 0.0)),
             length_pixels: -1.0,
             length_meters: -1.0,
@@ -37,8 +40,8 @@ impl Skeleton {
         }
     }
     pub fn project(&self, x: f32, y: f32) -> (f32, f32) {
-        let a = self.line[0];
-        let b = self.line[1];
+        let a = self.line_cvf[0];
+        let b = self.line_cvf[1];
         let (x1, y1) = (a.x, a.y);
         let (x2, y2) = (b.x, b.y);
         let (x_p, y_p) = (x, y);
@@ -74,9 +77,7 @@ impl Skeleton {
         }
     }
     pub fn draw_on_mat(&self, img: &mut Mat) {
-        let a = Point2i::new(self.line[0].x as i32, self.line[0].y as i32);
-        let b = Point2i::new(self.line[1].x as i32, self.line[1].y as i32);
-        match line(img, a, b, self.color, 2, LINE_8, 0) {
+        match line(img, self.line_cvi[0], self.line_cvi[1], self.color, 2, LINE_8, 0) {
             Ok(_) => {},
             Err(err) => {
                 panic!("Can't draw skeleton line for polygon due the error: {:?}", err)
