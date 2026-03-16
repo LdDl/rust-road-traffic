1cd9bb74a601b47a57759853c1c64bc220261c27
Dimitrii Lopanov <sexykdi@gmail.com>
2026-03-16 15:31:00 +0300

    fmt

--- src/lib/draw/primitives/pixel.rs
+++ src/lib/draw/primitives/pixel.rs
@@ -49,13 +49,7 @@ pub fn set_pixel(
 /// Out-of-bounds coordinates will cause a panic via slice indexing.
 /// Use this only after clipping coordinates to the image rectangle.
 #[inline]
-pub fn set_pixel_unchecked(
-    bytes: &mut [u8],
-    step: usize,
-    x: usize,
-    y: usize,
-    color: [u8; 3],
-) {
+pub fn set_pixel_unchecked(bytes: &mut [u8], step: usize, x: usize, y: usize, color: [u8; 3]) {
     let offset = y * step + x * 3;
     bytes[offset] = color[0];
     bytes[offset + 1] = color[1];
